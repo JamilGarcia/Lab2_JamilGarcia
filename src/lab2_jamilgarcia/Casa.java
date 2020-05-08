1b957fa23c083016503f4cc9a140781db87d20e9
JamilGarcia <you@example.com>
2020-05-08 13:47:42 -0600

    Menu terminado y login terminado

---
+++ src/lab2_jamilgarcia/Casa.java
@@ -0,0 +1,130 @@
+package lab2_jamilgarcia;
+
+public class Casa {
+    
+    private int n_casa; 
+    private int n_bloque; 
+    private String color; 
+    private int ancho; 
+    private int largo; 
+    private String comprada; 
+    private int pisos; 
+    private int banos; 
+    private int cuartos; 
+    private String dueño; 
+    private String ingeniero; 
+    
+    public Casa(){
+        
+    }
+
+    public Casa(int n_casa, int n_bloque, String color, int ancho, int largo, String comprada, int pisos, int banos, int cuartos, String dueño, String ingeniero) {
+        this.n_casa = n_casa;
+        this.n_bloque = n_bloque;
+        this.color = color;
+        this.ancho = ancho;
+        this.largo = largo;
+        this.comprada = comprada;
+        this.pisos = pisos;
+        this.banos = banos;
+        this.cuartos = cuartos;
+        this.dueño = dueño;
+        this.ingeniero = ingeniero;
+    }
+
+    public int getN_casa() {
+        return n_casa;
+    }
+
+    public void setN_casa(int n_casa) {
+        this.n_casa = n_casa;
+    }
+
+    public int getN_bloque() {
+        return n_bloque;
+    }
+
+    public void setN_bloque(int n_bloque) {
+        this.n_bloque = n_bloque;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    public int getLargo() {
+        return largo;
+    }
+
+    public void setLargo(int largo) {
+        this.largo = largo;
+    }
+
+    public String getComprada() {
+        return comprada;
+    }
+
+    public void setComprada(String comprada) {
+        this.comprada = comprada;
+    }
+
+    public int getPisos() {
+        return pisos;
+    }
+
+    public void setPisos(int pisos) {
+        this.pisos = pisos;
+    }
+
+    public int getBanos() {
+        return banos;
+    }
+
+    public void setBanos(int banos) {
+        this.banos = banos;
+    }
+
+    public int getCuartos() {
+        return cuartos;
+    }
+
+    public void setCuartos(int cuartos) {
+        this.cuartos = cuartos;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+
+    public String getIngeniero() {
+        return ingeniero;
+    }
+
+    public void setIngeniero(String ingeniero) {
+        this.ingeniero = ingeniero;
+    }
+
+    @Override
+    public String toString() {
+        return "Casa{" + "n_casa=" + n_casa + ", n_bloque=" + n_bloque + ", color=" + color + ", ancho=" + ancho + ", largo=" + largo + ", comprada=" + comprada + ", pisos=" + pisos + ", banos=" + banos + ", cuartos=" + cuartos + ", due\u00f1o=" + dueño + ", ingeniero=" + ingeniero + '}';
+    }
+    
+    
+    
+}
--- src/lab2_jamilgarcia/Lab2_JamilGarcia.java
+++ src/lab2_jamilgarcia/Lab2_JamilGarcia.java
@@ -1,21 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2_jamilgarcia;
 
-/**
- *
- * @author JamilGarcía
- */
-public class Lab2_JamilGarcia {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Lab2_JamilGarcia {
+    
+    static Scanner jg = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        char resp = 's';
+        boolean login = false;
+        while (resp == 's'){
+            System.out.println(""
+                    + "Menu: \n"
+                    + "1. Resgitro de Casas \n"
+                    + "2. Manejo de Estados \n"
+                    + "3. Log In \n"
+                    + "0. Salir \n"
+                    + "Ingrese una opcion: \n" );
+            int opcion = jg.nextInt(); 
+            switch (opcion) {
+                case 1:
+                    System.out.println("Registro de Casas");
+                    if (login == true){
+                        
+                    }
+                    break;
+                case 2:
+                    System.out.println("Manejo de Estasdos");
+                    if (login == true){
+                        
+                    }
+                    break;
+                case 3:
+                    System.out.println("Log In");
+                    System.out.println("Ingrese el usuario: ");
+                    String usua = jg.next();
+                    while (!"leobanegas".equals(usua)){
+                        System.out.println("Error!! usuario incorrecto!! Ingreselo de nuevo: ");
+                        usua = jg.next();
+                    }
+                    System.out.println("Ingrese contraseña: ");
+                    String pass = jg.next();
+                    while (!"99".equals(pass)){
+                        System.out.println("Error!! Contraseña incorrecta!! Ingreselo de nuevo: ");
+                        pass = jg.next();
+                    }
+                    break;
+                case 0:
+                    System.out.println("Gracias por utilizar el programa <3");
+                    break;
+                default:
+                    System.out.println("Ingrese un valor valido!!");
+            }
+        }
     }
     
 }
